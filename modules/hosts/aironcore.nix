ff954ea9c56111eb6d7d417a2f23c1c95d9fd7d5
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2025-12-04 22:35:29 +0000

    Trying to make cuda work on aironcore

--- modules/hosts/aironcore.nix
+++ modules/hosts/aironcore.nix
@@ -13,6 +13,39 @@ in {
       iron
       dev
       vim
+
+      ({pkgs, ...}: {
+        nixpkgs.config.allowUnfree = true;
+        services.ollama = {
+          enable = true;
+          package = pkgs.ollama-cuda;
+          acceleration = "cuda";
+          host = "0.0.0.0";
+          environmentVariables = {
+            OLLAMA_CONTEXT_LENGTH = "25000";
+            OLLAMA_MAX_LOADED_MODELS = "2";
+            OLLAMA_MAX_QUEUE = "512";
+          };
+        };
+
+        home.packages = with pkgs; [
+          vulkan-headers
+          vulkan-loader
+          cudaPackages.cudatoolkit
+          #cudaPackages.cudnn
+          cudaPackages.nccl
+          cudaPackages.cuda_cccl
+        ];
+        programs.zsh.shellAliases = {
+          btop = "/usr/bin/btop";
+        };
+        home.sessionVariables = {
+          LD_PRELOAD = "/usr/lib/x86_64-linux-gnu/libcuda.so.1:/usr/lib/x86_64-linux-gnu/libnvidia-ptxjitcompiler.so.1";
+          # LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.vulkan-loader}/lib:${pkgs.cudatoolkit}/lib:${pkgs.cudaPackages.nccl}/lib:/usr/lib/x86_64-linux-gnu/libcuda.so.1:/usr/lib/x86_64-linux-gnu/libnvidia-ptxjitcompiler.so.1";
+          CUDA_PATH = "${pkgs.cudatoolkit}";
+          VULKAN_SDK = "${pkgs.vulkan-headers}";
+        };
+      })
       network
       {
         hardware.gpu = "cuda";
