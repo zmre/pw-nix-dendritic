a28c1c2103e670e57830da8b7cd8e999a6daf8af
Patrick Walsh <zmre@mistgate.org>
2026-03-17 14:07:38 -0600

    Allow SFTP

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -3,16 +3,20 @@
     # Enable the OpenSSH daemon.
     services.openssh = {
       enable = true;
-      allowSFTP = false;
+      allowSFTP = true;
       openFirewall = true;
       settings = {
         # Allow public key authentication
         PubkeyAuthentication = true;
         # Disable password auth for security (optional but recommended)
         PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = ["pwalsh"];
+        Subsystem = "sftp internal-sftp ${pkgs.openssh}/libexec/sftp-server";
       };
     };
     programs.mosh.enable = false;
-    programs.mosh.openFirewall = true;
+    programs.mosh.openFirewall = false;
   };
 }
