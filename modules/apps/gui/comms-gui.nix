747e7818f728c11e582a29fc79c94bfe6ac4995f
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2026-08-10 11:09:51 -0600

    Add script to get links to mail messages

--- modules/apps/gui/comms-gui.nix
+++ modules/apps/gui/comms-gui.nix
@@ -13,6 +13,17 @@
     };
   };
 
+  flake.modules.homeManager.comms-gui = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    # Mail.app script menu is macOS-only
+    home.file = lib.optionalAttrs pkgs.stdenv.isDarwin {
+      "Library/Scripts/Applications/Mail/mail-app-copy-message-link.scpt".source = ../../../dotfiles/scripts/mail-app-copy-message-link-compiled.scpt;
+    };
+  };
+
   flake.nixosModules.comms-gui = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
       slack
